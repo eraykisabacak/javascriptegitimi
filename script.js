f722505bb37712db056083f0eca3f4a851c485c6
Eray Kısabacak <nefret19981998@hotmail.com>
2018-05-09 11:23:18 +0300

    9mayıs2018

---
+++ script.js
@@ -0,0 +1,7 @@
+
+		var e = document.getElementById("udemy").innerHTML;
+		console.log(e);
+
+		var ad = document.getElementById("ad").innerHTML;
+		var soyad = document.getElementById("soyad").innerHTML;
+		console.log(ad+soyad);
